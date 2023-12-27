96e03676ccaff5850e720c92c3f1bc36129f4c58
Avinash Tiwari <aviprf10@gmail.com>
2023-12-27 06:53:27 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const initialDictionary = [
+    { term: 'React', definition: 'A JavaScript library for building user interfaces' },
+    { term: 'Node.js', definition: 'A JavaScript runtime built on Chrome\'s V8 JavaScript engine' },
+    // Add more words and their meanings to the dictionary
+  ];
+
+  const [dictionary, setDictionary] = useState(initialDictionary);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResult, setSearchResult] = useState(null);
+  const [searchMessage, setSearchMessage] = useState('');
+
+  const handleSearch = () => {
+    // Implement search logic here
+    const result = dictionary.find(
+      (entry) => entry.term.toLowerCase() === searchTerm.toLowerCase()
+    );
+
+    // Update the search result and message state
+    setSearchResult(result);
+
+    if (result) {
+      setSearchMessage('');
+    } else {
+      setSearchMessage('Word not found in the dictionary.');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="Enter search term"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+
+      {searchResult && (
+        <div>
+          <h3>{searchResult.term}</h3>
+          <p>{searchResult.definition}</p>
+        </div>
+      )}
+
+      {searchMessage && <p>{searchMessage}</p>}
     </div>
   );
-}
+};
 
 export default App;
